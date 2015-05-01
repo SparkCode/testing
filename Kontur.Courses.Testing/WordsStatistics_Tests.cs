b0f62bcd1816f05559ab2466f8a35a44bc2ca355
SparkCode <iseb@outlook.com>
2015-05-01 22:40:00 +0500

    task WordsStatistics complete, without 6, 8, 13 programs

--- Kontur.Courses.Testing/WordsStatistics_Tests.cs
+++ Kontur.Courses.Testing/WordsStatistics_Tests.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Kontur.Courses.Testing.Implementations;
 using NUnit.Framework;
 
@@ -45,5 +47,77 @@ namespace Kontur.Courses.Testing
 			stat.AddWord("world");
 			CollectionAssert.AreEqual(new[] { Tuple.Create(2, "world"), Tuple.Create(1, "hello") }, stat.GetStatistics());
 		}
+
+	    [Test]
+	    public void order_words_in_case_equal_frequency()
+	    {
+	        stat.AddWord("b");
+            stat.AddWord("a");
+            stat.AddWord("c");
+            CollectionAssert.AreEqual(new[] {Tuple.Create(1, "a"), Tuple.Create(1, "b"), Tuple.Create(1, "c")}, stat.GetStatistics());
+	    }
+
+	    [Test]
+	    public void word_in_various_register()
+	    {
+            stat.AddWord("Aba");
+            stat.AddWord("aBA");
+            stat.AddWord("aba");
+            CollectionAssert.AreEqual(new[] {Tuple.Create(3, "aba")}, stat.GetStatistics());
+	    }
+
+        [Test]
+	    public void word_in_case_length_more_10()
+	    {
+            var baseWord = new string(Enumerable.Repeat('a', 10).ToArray());
+            stat.AddWord(baseWord + 'a');
+            CollectionAssert.AreEqual(new[] {Tuple.Create(1, baseWord)}, stat.GetStatistics());
+	    }
+
+	    [Test]
+        public void words_in_case_length_more_10_and_difference_in_11_position()
+	    {
+	        var baseWord = new string(Enumerable.Repeat('a', 10).ToArray());
+
+            stat.AddWord(baseWord + 'a');
+            stat.AddWord(baseWord + 'b');
+
+            CollectionAssert.AreEqual(new[] { Tuple.Create(2, baseWord)}, stat.GetStatistics());
+	    }
+
+	    [Test]
+        public void words_in_case_german_chars_in_various_register()
+	    {
+            stat.AddWord("Ä");
+            stat.AddWord("ä");
+
+            CollectionAssert.AreEqual(new[] {Tuple.Create(2, "ä")}, stat.GetStatistics());
+	    }
+
+	    [Test]
+        public void two_another_IWordsStatistics()
+        {
+            stat.AddWord("aba");
+
+            var stat2 = createStat();
+
+            CollectionAssert.AreEqual(new[] { Tuple.Create(1, "aba") }, stat.GetStatistics());
+        }
+
+	    [Test]
+	    public void null_instead_string()
+	    {
+	        stat.AddWord(null);
+
+            Assert.AreEqual(0, stat.GetStatistics().Count());
+	    }
+
+        [Test]
+        public void empty_string()
+        {
+            stat.AddWord("");
+
+            Assert.AreEqual(0, stat.GetStatistics().Count());
+        }
 	}
 }
